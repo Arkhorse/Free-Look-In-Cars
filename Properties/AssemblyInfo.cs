1fe71214abf806fc0f142f28bed5266945fe1fb5
Arkhorse <theillusivemman@gmail.com>
2023-05-10 15:37:18 -0300

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,8 +1,4 @@
-﻿using MelonLoader;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("FreeLookInCars")]
+﻿[assembly: AssemblyTitle("FreeLookInCars")]
 [assembly: AssemblyDescription("Allows the player to look freely in cars")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -11,8 +7,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("bc320585-8c10-4ece-99a4-1c57d170e828")]
-[assembly: AssemblyVersion("4.0.0.1")]
-[assembly: AssemblyFileVersion("4.0.0.1")]
-[assembly: MelonInfo(typeof(FreeLookInCars.Implementation), "FreeLookInCars", "4.0.1", "WulfMarius, zeobviouslyfakeacc, The Illusion")]
+
+[assembly: AssemblyVersion("4.0.2.0")]
+[assembly: AssemblyFileVersion("4.0.2.0")]
+[assembly: MelonInfo(typeof(FreeLookInCars.FreeLookInCars), "FreeLookInCars", "4.0.2", "WulfMarius, zeobviouslyfakeacc, The Illusion")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
